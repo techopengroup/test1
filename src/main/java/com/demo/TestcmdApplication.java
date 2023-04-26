08d334cde3832972c48868e59f4c5af04756f985
techopengroup <techopengroup@gmail.com>
2023-04-26 23:16:39 +0530

    first commit cmd

---
+++ src/main/java/com/demo/TestcmdApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcmdApplication.class, args);
+	}
+
+}
