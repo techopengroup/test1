a2623637fd5227089d498c061a4ac8690a68dc96
techopengroup <130648038+techopengroup@users.noreply.github.com>
2023-04-26 23:08:38 +0530

    Add files via upload

---
+++ testcmd/src/main/java/com/demo/Test.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Just Github Commit");
+		System.out.println("Just Test in cmd Github Commit");
+	}
+
+}
